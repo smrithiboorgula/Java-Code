3252a2451e48b0ab73348f6d11ef9aea99f54d4b
smrithiboorgula <smrithiboorgula06@gmail.com>
2025-01-25 12:45:48 +0530

    print 5 table

---
+++ Demo050.java
@@ -0,0 +1,8 @@
+public class Demo050 {
+    public static void main(String[] args) {
+        int number = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+}
