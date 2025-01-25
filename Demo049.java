43a1437fb2d126e7335cf6f1861f77890671c354
smrithiboorgula <smrithiboorgula06@gmail.com>
2025-01-25 12:39:19 +0530

    Reverse a number

---
+++ Demo049.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Demo049 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        
+        int reversed = 0;
+        while (num != 0) {
+            int digit = num % 10;
+            reversed = reversed * 10 + digit;
+            num /= 10;
+        }
+        
+        System.out.println("Reversed number: " + reversed);
+        
+        scanner.close();
+    }
+}
