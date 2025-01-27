768288b7713a0c2112a6fd6d193ec95856281940
smrithiboorgula <smrithiboorgula06@gmail.com>
2025-01-27 10:56:08 +0530

    To check number even or not

---
+++ Demo034.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Demo034 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+        scanner.close();
+    }
+}
