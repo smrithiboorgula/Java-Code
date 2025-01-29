43447072ba09e2369c19e68e15a10266fddf9286
smrithiboorgula <smrithiboorgula06@gmail.com>
2025-01-29 12:32:55 +0530

    Merge Sorted Arrays

---
+++ LTCode008.java
@@ -0,0 +1,19 @@
+class LTCode008 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m - 1, j = n - 1, k = m + n - 1; j >= 0; --k) {
+            nums1[k] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+        }
+    }
+
+    public static void main(String[] args) {
+        LTCode008 solution = new LTCode008();
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        solution.merge(nums1, m, nums2, n);
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+}
