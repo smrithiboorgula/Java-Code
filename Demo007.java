68e84a26183faae539472610d59fbd7acccf5404
smrithiboorgula <smrithiboorgula06@gmail.com>
2025-02-03 20:41:15 +0530

    user input

---
+++ Demo007.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Demo007 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter user, age, phone number: ");
+        String name = sc.next();
+        String age = sc.next();
+        String num = sc.next();
+        System.out.println("Hello "+name+", age = "+age+", phone number: "+num);
+    }
+}
