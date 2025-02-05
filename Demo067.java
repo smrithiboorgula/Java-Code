bf3fb2b3186e17d6bd8ab798936483c3da6c4fb9
smrithiboorgula <smrithiboorgula06@gmail.com>
2025-02-05 12:35:30 +0530

    print even elements of an array

---
+++ Demo067.java
@@ -0,0 +1,10 @@
+public class Demo067 {
+    public static void main(String[] args) {
+        int[] arr = {10, 15, 20, 25, 30, 35};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
