53bed63b2ba5337045a7f016488f07b56f403176
smrithiboorgula <smrithiboorgula06@gmail.com>
2025-02-05 12:37:02 +0530

    print sum of all the elements of an array

---
+++ Demo069.java
@@ -0,0 +1,10 @@
+public class Demo069 {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println("Sum of all elements: " + sum);
+    }
+}
